c84ec6b9d481813e52b96d7e83d51adc98ad13e5
kousiknandy <kousiknandy@users.noreply.github.com>
2022-04-01 22:48:40 +0100

    optimize on space
    as we're increasing there is no need to retain previous values

--- 50.py
+++ 50.py
@@ -5,28 +5,16 @@ with open(sys.argv[1]) as p_file:
 primes = primes.split(",")
 primes = [int(x) for x in primes]
 primeset = frozenset(primes)
-sumof = [0] * len(primes)
 # sum of 546 smallest primes exceeds a million,
 # so that can be biggest sliding window size
+m, v = 0, 0
 for window in range(546, 1, -1):
     wsum = sum(primes[:window])
-    if wsum in primeset:
-        idx = primes.index(wsum)
-        sumof[idx] = max(sumof[idx], window)
-    # slide the window till the sum exceeds
+    if wsum in primeset: v, m = wsum, window
+    # keep sliding the window till sum exceeds
     for st in range(1,len(primes)):
-        wsum -= primes[st-1]
-        wsum += primes[st+window-1]
+        wsum += primes[st+window-1] - primes[st-1]
         if wsum > 10**6: break
-        if wsum in primeset:
-            idx = primes.index(wsum)
-            sumof[idx] = max(sumof[idx], window)
-    if any(sumof):
-        break
-m, v = 0, 0
-for j in range(len(primes)-1, -1, -1):
-    if sumof[j] > m:
-        m = sumof[j]
-        v = primes[j]
+        if wsum in primeset:v, m = wsum, window
+    if v: break
 print(v, m)
-

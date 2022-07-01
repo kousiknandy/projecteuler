fc7102a99b98525e4babd2aa208cc22d60feaff6
kousiknandy <kousiknandy@users.noreply.github.com>
2022-07-01 16:42:54 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
->>> f1 = lambda x, y: x*y*(y+1)//2
->>> f2 = lambda x: f1(x,999//x)
->>> f3 = lambda x, y: f2(x) + f2(y) - f2(x*y)
->>> f3(3,5)
-233168
+f1 = lambda x, y: x*y*(y+1)//2
+f2 = lambda x: f1(x,999//x)
+f3 = lambda x, y: f2(x) + f2(y) - f2(x*y)
+f3(3,5)
+# 233168
